57c7771039dc3d83bba95da700437dffeecf2562
PG_Axis <axis.sixtikcz@gmail.com>
2025-09-13 12:35:50 +0200

    Add files via upload

--- YTCnv/Platforms/Android/MainActivity.cs
+++ YTCnv/Platforms/Android/MainActivity.cs
@@ -50,6 +50,65 @@ namespace YTCnv
             HandleIntent(intent);
         }
 
+        public static void PickFolder(Activity activity, int requestCode = 42)
+        {
+            Intent intent = new Intent(Intent.ActionOpenDocumentTree);
+            intent.AddFlags(ActivityFlags.GrantPersistableUriPermission |
+                            ActivityFlags.GrantReadUriPermission |
+                            ActivityFlags.GrantWriteUriPermission);
+
+            activity.StartActivityForResult(intent, requestCode);
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == 42 && resultCode == Result.Ok)
+            {
+                Android.Net.Uri treeUri = data.Data;
+
+                var flags = data.Flags & (ActivityFlags.GrantReadUriPermission | ActivityFlags.GrantWriteUriPermission);
+                ContentResolver.TakePersistableUriPermission(treeUri, flags);
+
+                settings.FileUri = treeUri.ToString();
+                SaveFileNames(settings.FileUri);
+            }
+        }
+
+        private void SaveFileNames(string UriString)
+        {
+            if (UriString != null)
+            {
+                var treeUri = Android.Net.Uri.Parse(UriString);
+                var (root, folder) = GetFriendlyFolderName(treeUri);
+
+                settings.MainFolder = root;
+                settings.FinalFolder = $" - {folder}" ?? "";
+            }
+        }
+
+        public static (string Root, string? EndFolder) GetFriendlyFolderName(Android.Net.Uri treeUri)
+        {
+            var docId = Android.Provider.DocumentsContract.GetTreeDocumentId(treeUri);
+            var parts = docId.Split(':');
+
+            string root;
+            if (parts[0].Equals("primary", StringComparison.OrdinalIgnoreCase))
+                root = "Internal storage";
+            else
+                root = "SD card";
+
+            string? endFolder = "";
+            if (parts.Length > 1 && !string.IsNullOrEmpty(parts[1]))
+            {
+                var subPath = parts[1];
+                var segments = subPath.Split('/');
+                endFolder = segments.Last();
+            }
+            return (root, endFolder);
+        }
+
         private void HandleIntent(Intent? intent)
         {
             if (intent == null) return;
